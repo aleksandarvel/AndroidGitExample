ec9eb952f8a92df5380017894d863948781f711b
aleksandarvel <avelinov2@gmail.com>
2015-12-23 11:53:12 +0100

    4 commit

--- app/src/main/java/com/example/ace/androidgitexample/MainActivity.java
+++ app/src/main/java/com/example/ace/androidgitexample/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b2 =new Button(getBaseContext());
+        Button b3 =new Button(getBaseContext());
+
     }
 }
